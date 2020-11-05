9615eca8d5cdb010a1a58b7fd59110c14abf9851
mmm545 <allhammhmd83@gmail.com>
2020-11-05 17:59:47 +0300

    Added some getters and setters

--- src/mmm545/libgamespyquery/GameSpyQuery.php
+++ src/mmm545/libgamespyquery/GameSpyQuery.php
@@ -149,4 +149,32 @@ class GameSpyQuery
 
         return "";
     }
+
+    /**
+     * @return string
+     */
+    public function getIp(): string{
+        return $this->ip;
+    }
+
+    /**
+     * @param string $ip
+     */
+    public function setIp(string $ip): void{
+        $this->ip = $ip;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPort(): int{
+        return $this->port;
+    }
+
+    /**
+     * @param int $port
+     */
+    public function setPort(int $port): void{
+        $this->port = $port;
+    }
 }
